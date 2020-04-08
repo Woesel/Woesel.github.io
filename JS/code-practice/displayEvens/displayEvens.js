7b93ef6cdef7c7776b8c403ce871b11311da6e38
Tenzin Woesel <tenz_woesel@yahoo.com>
2020-04-08 19:23:52 -0400

    fixed display evens

--- JS/code-practice/displayEvens/displayEvens.js
+++ JS/code-practice/displayEvens/displayEvens.js
@@ -1,26 +1,32 @@
+  let num1;
+  let num2;
+  let step;
+
+
 function displayEvens() {
 
   document.getElementById("emptyStart").style.display = "none";
   document.getElementById("emptyEnd").style.display = "none";
   document.getElementById("emptyStep").style.display = "none";
 
-  let num1 = Number(document.getElementById("startNum").value);
-  let num2 = Number(document.getElementById("endNum").value);
-  let step = Number(document.getElementById("step").value);
-
-  document.getElementById("resultText").innerHTML = "Here are the even numbers between " + num1 + " and " + num2 + " incremented by " + step + ":";
+  num1 = Number(document.getElementById("startNum").value);
+  num2 = Number(document.getElementById("endNum").value);
+  step = Number(document.getElementById("step").value);
 
   if (num1 == "") {
     document.getElementById("emptyStart").style.display = "block";
+    return false;
 
-  } else if (num2 == "") {
+  }if (num2 == "") {
     document.getElementById("emptyEnd").style.display = "block";
+    return false;
 
   } else if (step == "") {
     document.getElementById("emptyStep").style.display = "block";
-    return;
+    return false;
   }
 
+  document.getElementById("resultText").innerHTML = "Here are the even numbers between " + num1 + " and " + num2 + " incremented by " + step + ":";
 
 
 
@@ -29,23 +35,21 @@ function displayEvens() {
 
   } else if (step < 0) {
     alert("Please use a positive number ");
+    return false;
   } else if (step > num1 && step > num2) {
     alert("In the Step field, please use a number lesser than Starting number and ending number ");
+    return false;
   } else if (num1 % 2 !== 0 && step % 2 == 0) {
     alert("There is no even number is the specified value. Change starting number or the step.");
+    return false;
   }
 
   let answer = "";
 
-
   for (let i = num1; i <= num2; i += step) {
-
-
-
     if (i % 2 == 0) {
       answer = answer + i + "<br>";
     }
-
   }
   document.getElementById("answer").innerHTML = answer;
   document.getElementById("showResult").style.display = "block";
