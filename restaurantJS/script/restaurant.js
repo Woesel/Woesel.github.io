9dece6c95a9c5f715283394e0b887b723a5d833d
Tenzin Woesel <tenz_woesel@yahoo.com>
2020-04-10 04:57:34 -0400

    restaurantJS finished

---
+++ restaurantJS/script/restaurant.js
@@ -0,0 +1,21 @@
+function validateForm() {
+
+  let name = document.getElementById("name1").value;
+  // let email = document.getElementById("email").value;
+  // let phone = Number(document.getElementById("phone").value);
+  // console.log(name);
+  if (name == "") {
+    document.getElementById("error1").style.display = "block";
+    return false;
+    //  alert("Please enter your name");
+  }
+  // if (email == "") {
+  //   document.getElementById("error1").style.display = "block";
+  //   return false;
+  // }
+  // if (phone == "") {
+  //   document.getElementById("error1").style.display = "block";
+  //   return false;
+  // }
+return false;
+}
