550530252406f4359d651913c21bab19a50929bb
Tenzin Woesel <tenz_woesel@yahoo.com>
2020-04-11 21:56:38 -0400

    added error message to form

--- restaurantJS/script/restaurant.js
+++ restaurantJS/script/restaurant.js
@@ -1,21 +1,24 @@
 function validateForm() {
 
   let name = document.getElementById("name1").value;
-  // let email = document.getElementById("email").value;
-  // let phone = Number(document.getElementById("phone").value);
+  let email = document.getElementById("email").value;
+  let phone = Number(document.getElementById("phone").value);
   // console.log(name);
-  if (name == "") {
-    document.getElementById("error1").style.display = "block";
-    return false;
-    //  alert("Please enter your name");
+  if (name == ""|| email == ""|| phone == "") {
+    if(name==""){
+      document.getElementById("error1").style.display = "block";
+
+    }
+    if(email== ""){
+      document.getElementById("error2").style.display = "block";
+
+	}if(phone == ""){
+		document.getElementById("error3").style.display = "block";
+
+    }
+    return false;  //  alert("Please enter your name");
+  }else{
+    alert("Form submitted successfully");
   }
-  // if (email == "") {
-  //   document.getElementById("error1").style.display = "block";
-  //   return false;
-  // }
-  // if (phone == "") {
-  //   document.getElementById("error1").style.display = "block";
-  //   return false;
-  // }
-return false;
+  return false;
 }
